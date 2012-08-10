49a939393a985f72e53973a2f7557a0e65624d56
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2012-08-10 11:44:46 +0200

    Simple field selector in toolbox

--- js/templates/jst.js
+++ js/templates/jst.js
@@ -9,9 +9,17 @@
 
 window.JST={};
 
-window.JST['editor/tools'] = _.template('\
-     Mouse Left: <span id="button1"> Button </span> <span id="drag1"> Drag </span><br />\
-     Mouse Middle: <span id="button2"> Button </span> <span id="drag2"> Drag </span><br />\
-     Mouse Right: <span id="button3"> Button </span> <span id="drag3"> Drag </span>\
-     <div id="size-slider" style="margin: 5px"></div>\
-');
+window.JST['editor/tools'] = function() {
+	var html='\
+	Mouse Left: <span id="button1"> Button </span> <span id="drag1"> Drag </span><br />\
+	Mouse Middle: <span id="button2"> Button </span> <span id="drag2"> Drag </span><br />\
+	Mouse Right: <span id="button3"> Button </span> <span id="drag3"> Drag </span>\
+	<div id="size-slider" style="margin: 5px"></div>\
+	';
+	var fields=["O","X","Y","Z",1,2,3,4,5,6,7,8,9,"P","S","F"]
+	var offset=42;
+	for (var i=0; i<fields.length;i++) {
+		html+='<div class="fieldSelector" field="'+fields[i]+'" style="background-position: -'+(i*offset)+'px 0"></div>';
+	}
+	return _.template(html);
+}
