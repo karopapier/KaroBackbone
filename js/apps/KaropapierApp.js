5f93ffdab443421bb20595edf512af22ded0a2bd
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2014-05-20 17:10:03 +0200

    Added KaropapierApp

---
+++ js/apps/KaropapierApp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by pdietrich on 20.05.14.
+ */
+
+
+var KaropapierApp = Backbone.Marionette.Layout.extend({
+    //global layout with regions for nav, sidebar, header and user info...
+
+})
