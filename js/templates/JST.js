4d11ad35d693f684d5d583b65bc414fc7aab0acc
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2014-05-21 13:06:58 +0200

    Dynamic Template test

--- js/templates/JST.js
+++ js/templates/JST.js
@@ -2,4 +2,4 @@ this["JST"] = this["JST"] || {};
 
 this["JST"]["basic/basic.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += '<div id="header">Header</div>\r\n<div id="content">Content</div>\r\n<div id="footer">Footer</div>';}return __p};
 
-this["JST"]["dumbview.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += 'Hello, I am a <strong>dumb</strong> view template. Please JST me via grunt an watch my changes, so I can regrunt';}return __p};
+this["JST"]["dumbview.html"] = function(obj) {obj || (obj = {});var __t, __p = '', __e = _.escape;with (obj) {__p += 'Hello, I am a <strong>' +((__t = ( adjective )) == null ? '' : __t) +'</strong> view template. Please JST me via grunt an watch my changes, so I can regrunt!\r\n';}return __p};
