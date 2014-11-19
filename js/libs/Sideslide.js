75a69f994011e472c20a1b8b1ba26bbd65a11519
KaroDidi <didi@karopapier.de>
2014-11-19 16:13:47 +0100

    Panzoom/Sideslide WIP

--- js/libs/Sideslide.js
+++ js/libs/Sideslide.js
@@ -3,34 +3,60 @@ var Sideslide = function (el, options) {
         options = options || {};
         me.el = el;
         me.$el = $(el);
-        me.borderPercent = .2;
+        me.borderPercent = .1;
         me.isActive = false;
         me.isVisible = false;
+		me.ignore = false;
+		me.notSure = true;
         me.side = options.side || "right";
         me.rightSide = (me.side == "right");
+		me.sw = me.$el.width();
         me.x = 0;
 
         me.recalc = function () {
-            me.dw = $(document).width();
-            me.sw = me.$el.width();
+			var ori = window.orientation || 0;
+			//console.log("Orient " + window.orientation);
+			//console.log("wW wH " + $(window).width() + " " + $(window).height());
+			//console.log("dW dH " + $(document).width() + " " + $(document).height());
+			//console.log("iW iH " + window.innerWidth + " " + window.innerHeight);
+			var $dot = $("<div>");
+			$("body").append($dot);
+			$dot.css({
+				right: 0,
+				left:'',
+				position: "absolute",
+				width: 1,
+				height: 1,
+				backgroundColor: "yellow"
+			});
+			//console.log($dot);
+			//console.log($dot.offset());
+			var dotpos =$dot.offset().left + 1;
+			$dot.remove();
+			//console.log("dot: " + dotpos);
+			me.dw = dotpos;
+			//console.log("New width: " + me.dw);
+
         }
 
         me.rotate = function () {
-            console.log("Rotating, " + me.el.id + " isVisible " + me.isVisible);
+            //console.log("Rotating, " + me.el.id + " isVisible " + me.isVisible);
             me.recalc();
+			me.hide();
+			return true;
+
             if (me.isVisible) {
-                console.log("OPEN", me.el.id);
+                //console.log("OPEN", me.el.id);
                 me.slideTo(me.openX());
             } else {
-                console.log("CLOSE", me.el.id);
+                //console.log("CLOSE", me.el.id);
                 me.slideOut();
             }
         }
 
-        me.recalc();
 
         me.openX = function () {
-            console.log(me.el.id + "is right: " + me.rightSide);
+            //console.log(me.el.id + "is right: " + me.rightSide);
             if (me.rightSide) {
                 return me.dw - me.sw
             } else {
@@ -48,32 +74,38 @@ var Sideslide = function (el, options) {
 
         me.slideIn = function () {
             me.slideTo(me.openX(),true);
-            console.log("DA " + me.el.id);
+            //console.log("DA " + me.el.id);
             me.isVisible = true;
         };
 
         me.slideOut = function () {
-            me.slideTo(me.closeX(), true);
-            console.log("WEG " + me.el.id);
-            me.isVisible = false;
-            me.$el.hide();
+            me.slideTo(me.closeX(), true, me.hide);
+            //console.log("WEG " + me.el.id);
+            ////me.hide();
         };
 
-        me.slideTo = function (x, animate) {
+		me.hide = function() {
+            me.isVisible = false;
+			me.$el.hide();
+		};
+
+        me.slideTo = function (x, animate, cb) {
             animate = animate || false;
             if (!me.rightSide) {
                 x = x - me.sw;
             }
+			me.$el.show();
+			me.$el.css({"position":"fixed", top: 10});
             if (animate) {
                 me.$el.animate({
                     left: x
-                })
+                }, {complete: cb});
             } else {
                 me.$el.css({
                     left: x
                 });
             }
-            console.log("Slide to " + x);
+            //console.log("Slide to " + x);
         };
 
         me.isPulledOut = function () {
@@ -87,24 +119,42 @@ var Sideslide = function (el, options) {
 
         me.handleStart = function (e) {
             var x = e.center.x;
-            var b = me.dw * (1 - me.borderPercent);
-
-            console.log(e.type + " " + e.center.x);
-            console.log(x + " > " + b);
+            console.log(e.type + " " + x);
+			//console.log(me.dw);
+			me.ignore = false;
+			me.notSure = true;
 
             if (me.rightSide) {
                 me.isActive = (x > me.dw * (1 - me.borderPercent));
             } else {
                 me.isActive = (x < me.dw * me.borderPercent);
             }
-            if (me.isActive || me.isVisible) {
-                me.$el.show();
+
+			if (me.isActive && e.type=="press") {
                 me.slideTo(x);
-                me.isVisible = true;
-            }
+			}
+            //if (me.isActive || me.isVisible) {
+                //me.$el.show();
+                //me.isVisible = true;
+            //}
         };
 
         me.handleDrag = function (e) {
+			console.log("Drag");
+			if (me.ignore) {
+				//console.log("ign");
+				return true;
+			}
+			//console.log(e.deltaX + " " + e.deltaY);
+			if (me.notSure) {
+				me.notSure = false;
+				//console.log("Checking ONCE");
+				if (Math.abs(e.deltaY)>Math.abs(e.deltaX)) {
+					me.ignore=true;
+					//console.log("wrong direction");
+					return false;
+				}
+			}
             if (me.isVisible || me.isActive) {
                 var x = e.center.x;
                 //only pull out max width
@@ -120,16 +170,22 @@ var Sideslide = function (el, options) {
                 me.x = x;
                 me.slideTo(x);
                 e.preventDefault();
-            }
+            } else {
+				//console.log("IGNORE");
+			}
         };
 
         me.handleEnd = function (e) {
+			if (me.ignore) {
+				//console.log("ign");
+				return true;
+			}
             if (!(me.isActive || me.isVisible)) {
                 return true;
             }
 
             //only if active
-            console.log(e.type + " " + e.center.x);
+            //console.log(e.type + " " + e.center.x);
             me.isActive = false;
             var x = e.center.x;
 
@@ -158,19 +214,26 @@ var Sideslide = function (el, options) {
             } else {
                 me.slideOut();
             }
-            console.log("Now " + me.el.id + " act, vis", me.isActive, me.isVisible);
+            //console.log("Now " + me.el.id + " act, vis", me.isActive, me.isVisible);
         }
 
 //init
         var hammerOptions = {};
         var hammertime = new Hammer(document.body, hammerOptions);
-        hammertime.get('pan').set({direction: Hammer.DIRECTION_HORIZONTAL});
+        hammertime.get('pan').set({direction: Hammer.DIRECTION_ALL});
         hammertime.on("press", me.handleStart);
         hammertime.on("panstart", me.handleStart);
         hammertime.on('pan', me.handleDrag);
         hammertime.on("panend", me.handleEnd);
 
+        me.recalc();
+		me.hide();
+
         var supportsOrientationChange = "onorientationchange" in window, orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
-        window.addEventListener(orientationEvent, me.rotate);
-    }
-    ;
+        window.addEventListener(orientationEvent, function() {
+			me.hide();
+			setTimeout(function() {
+				me.rotate();
+			},700);
+		});
+    } ;
