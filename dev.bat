58ec4b1d3d90c4735a979334f0ae69689d03d135
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2015-01-15 17:50:08 +0100

    dev.bat

---
+++ dev.bat
@@ -0,0 +1,4 @@
+start cmd /k "mode 80,30 & grunt"
+start cmd /k "mode 80,30 & servedir ."
+start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login
+start "" "C:\Program Files (x86)\Vim\vim73\gvim.exe" "D:\TMP\gh\KaroBackbone\index.html"
