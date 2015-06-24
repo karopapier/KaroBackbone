98a3a3a1cd744aebe23edf586808112f9ff5e367
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2015-06-24 21:45:37 +0200

    No more vi in dev.bat

--- dev.bat
+++ dev.bat
@@ -1,4 +1,4 @@
 start cmd /k "mode 80,30 & grunt"
 start cmd /k "mode 80,10 & servedir . 2000"
 start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login
-start "" "C:\Program Files (x86)\Vim\vim73\gvim.exe" "D:\TMP\gh\KaroBackbone\index.html"
+rem start "" "C:\Program Files (x86)\Vim\vim73\gvim.exe" "D:\TMP\gh\KaroBackbone\index.html"
