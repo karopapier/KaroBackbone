6c9c9e55c197e0f0437ee444ae51f6c06ed9eea9
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2015-07-29 22:34:11 +0200

    Image scroll fix test

--- js/view/ChatMessageView.js
+++ js/view/ChatMessageView.js
@@ -12,11 +12,25 @@ var ChatMessageView = Backbone.View.extend({
     },
     render: function () {
         //var text = this.model.get("text");
+        var me = this;
         var data = this.model.toJSON();
         this.$el.html(this.model.get("text"));
         data.text = this.$el.text();
         data.text = KaroUtil.linkify(data.text);
         this.$el.html(this.template(data));
+        var imgs = this.$el.find("img").load(function (e) {
+            var $parparent = me.$el.parent().parent();
+            var newHeight = me.$el.height();
+            console.log("Message height changed from", messageHeight, "to", newHeight);
+            var old = $parparent.scrollTop();
+            var now = old + newHeight - messageHeight;
+            $parparent.scrollTop(now);
+            console.log("nachher", $parparent.scrollTop());
+        });
+        var messageHeight = -1;
+        setTimeout(function () {
+            messageHeight = me.$el.height();
+        }, 5);
         return this;
     }
 });
