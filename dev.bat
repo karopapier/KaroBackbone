b9038536417b692bc470211a6c878e5443d48239
Peter Dietrich <peter.dietrich@smartpixelmedia.de>
2016-02-03 13:26:12 +0100

    Pfanneneier

--- dev.bat
+++ dev.bat
@@ -1,4 +1,4 @@
 start cmd /k "mode 80,30 & grunt"
 start cmd /k "mode 80,10 & servedir public 2000"
-start "" "%SYSTEMDRIVE%\Program Files (x86)\Git\bin\sh.exe" --login
+start "" "%SYSTEMDRIVE%\Program Files\Git\git-bash.exe" --login
 rem start "" "C:\Program Files (x86)\Vim\vim73\gvim.exe" "D:\TMP\gh\KaroBackbone\index.html"
